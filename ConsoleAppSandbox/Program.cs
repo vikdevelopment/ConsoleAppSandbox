2fa4afadbace40138f66e8a9803253b32c0c5938
pavel.vik@greycorbel.com <pavel.vik@greycorbel.com>
2026-03-10 10:26:46 +0100

    add appsettings and test

--- ConsoleAppSandbox/Program.cs
+++ ConsoleAppSandbox/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!Version2");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        Console.WriteLine($"Environment: {context.HostingEnvironment.EnvironmentName}");
+        var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+        config
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .AddJsonFile($"appsettings.{environment}.json", optional: false)
+            .AddEnvironmentVariables();
+
+        var builtConfig = config.Build();
+    })
+    .Build();
+
+await RunAsync(host.Services);
+
+static async Task RunAsync(IServiceProvider services)
+{
+    //load variables:
+    var configuration = services.GetRequiredService<IConfiguration>();
+    var systemEnvironment = configuration["SYSTEM_ENVIRONMENT"];
+
+    var testValue = configuration["Test"];
+
+    Console.Write("Console application started successfully.");
+    Console.Write($"Variable test: {testValue}");
+
+    Console.Write("Console application finished successfully.");
+    await Task.CompletedTask;
+}
